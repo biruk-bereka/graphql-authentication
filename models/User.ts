a39ae89f33a2c95b6f363974a81fd89d280968a1
biruk-bereka <birukbeletech1212@gmail.com>
2023-08-11 16:31:49 +0300

    Add User model

---
+++ models/User.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    twoFactorAuth: Boolean,
+    secret: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
